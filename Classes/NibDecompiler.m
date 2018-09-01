bc0924f778fbd66350831d40129a834dedaa557d
Ian Gregory <justcheesy@outlook.com>
2018-09-01 13:00:42 -0400

    Clean up code a bit It's mostly the same

--- Classes/NibDecompiler.m
+++ Classes/NibDecompiler.m
@@ -13,41 +13,45 @@
 
 - (id)runWithInput:(id)input fromAction:(AMAction *)anAction error:(NSDictionary **)errorInfo
 {
-  if([input isKindOfClass:[NSArray class]])
+  if (![input isKindOfClass:[NSArray class]])
   {
-    NSFileManager * manager = [NSFileManager defaultManager];
-    NSBundle * bundle = [NSBundle bundleForClass:[self class]];
-    
-    for(id itemPath in input)
-    {
-      if([itemPath isKindOfClass:[NSString class]] && [[itemPath pathExtension] isEqualToString:@"nib"])
-      {
-        if(![[NSWorkspace sharedWorkspace] isFilePackageAtPath:itemPath])
-        {
-          NSString * keyedobjectsPathOld = [NSString stringWithFormat:@"%@/keyedobjects.nib", [itemPath stringByDeletingLastPathComponent]];
-          NSString * keyedobjectsPathNew = [NSString stringWithFormat:@"%@/keyedobjects.nib", itemPath];
-          [manager moveItemAtPath:itemPath toPath:keyedobjectsPathOld error:NULL];
-          [manager createDirectoryAtPath:itemPath withIntermediateDirectories:YES attributes:nil error:NULL];
-          [manager moveItemAtPath:keyedobjectsPathOld toPath:keyedobjectsPathNew error:NULL];
-        }
-        
-        NSString * classesNibPathNew = [NSString stringWithFormat:@"%@/classes.nib", itemPath];
-        NSString * infoNibPathNew = [NSString stringWithFormat:@"%@/info.nib", itemPath];
-        NSString * classesNibPathOld = [bundle pathForResource:@"classes" ofType:@"nib"];
-        NSString * infoNibPathOld = [bundle pathForResource:@"info" ofType:@"nib"];
-        
-        [manager copyItemAtPath:classesNibPathOld toPath:classesNibPathNew error:NULL];
-        [manager copyItemAtPath:infoNibPathOld toPath:infoNibPathNew error:NULL];
-      }
-      else
-      {
-        NSLog(@"Class of the item must be NSString (current classname is %@) or file is not nib file",[itemPath className]);
-      }
-    }
+    NSLog(@"Class of the input must be NSArray (current classname is %@)", [input className]);
+    return input;
   }
-  else
+  
+  NSFileManager *fileManager = [NSFileManager defaultManager];
+  NSWorkspace *workspace = [NSWorkspace sharedWorkspace];
+  NSBundle *bundle = self.bundle;
+  
+  for (NSString *itemPath in input)
   {
-    NSLog(@"Class of the input must be NSArray (current classname is %@)",[input className]);
+    if (![itemPath isKindOfClass:[NSString class]])
+    {
+      NSLog(@"Class of the item must be NSString (current classname is %@)", [itemPath className]);
+      continue;
+    }
+    if (![[itemPath pathExtension] isEqualToString:@"nib"])
+    {
+      NSLog(@"File %@ is not a nib file", itemPath);
+      continue;
+    }
+    
+    if (![workspace isFilePackageAtPath:itemPath])
+    {
+      NSString *keyedobjectsPathOld = [NSString stringWithFormat:@"%@/keyedobjects.nib", [itemPath stringByDeletingLastPathComponent]];
+      NSString *keyedobjectsPathNew = [NSString stringWithFormat:@"%@/keyedobjects.nib", itemPath];
+      [fileManager moveItemAtPath:itemPath toPath:keyedobjectsPathOld error:NULL];
+      [fileManager createDirectoryAtPath:itemPath withIntermediateDirectories:YES attributes:nil error:NULL];
+      [fileManager moveItemAtPath:keyedobjectsPathOld toPath:keyedobjectsPathNew error:NULL];
+    }
+    
+    NSString *classesNibPathNew = [NSString stringWithFormat:@"%@/classes.nib", itemPath];
+    NSString *infoNibPathNew = [NSString stringWithFormat:@"%@/info.nib", itemPath];
+    NSString *classesNibPathOld = [bundle pathForResource:@"classes" ofType:@"nib"];
+    NSString *infoNibPathOld = [bundle pathForResource:@"info" ofType:@"nib"];
+    
+    [fileManager copyItemAtPath:classesNibPathOld toPath:classesNibPathNew error:NULL];
+    [fileManager copyItemAtPath:infoNibPathOld toPath:infoNibPathNew error:NULL];
   }
 	
 	return input;
